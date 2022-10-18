301f52316f548db4abe7f7d082e4c2958634cc12
SBiscBoi <80650186+SBiscBoi@users.noreply.github.com>
2022-10-18 16:24:10 -0500

    changed purpose of script to only produce picture

--- main.py
+++ main.py
@@ -2,45 +2,18 @@ import random
 import sys
 from PIL import Image
 
-sys.set_int_max_str_digits(100000) #if i don't do this i get an error?
-
 width = 1920
 height = 1080
 
-input = "1" #input goes here for now
-
-num1 = 0
-num2 = 1
-
-#fibonacci time
-while len(input) != (width*height*3):
-    input = input + str(num1)
-    temp = num1 + num2
-    num1 = num2
-    num2 = temp
-    if len(input) > (width*height*3):
-        input = input[0:(width*height*3)] #shorten to (width*height)*3 length and break loop(hopefully)
-
-p = 0
-while p < len(input):
-    if int(input[p:(p+3)]) >= 256:
-        input = input[:p] + str(int(input[p:(p+3)]) % 255) + input[p+1:]
-    p += 3 #takes forever, might be optimizable
-p = 0 #reset to save some memory and not create new varible because this is about to be used again
-
-arr = [[0 for c in range(3)] for r in range(width*height)] #storage
-
-for row in range(len(arr)):
-    for col in range(len(arr[0])):
-        arr[row][col] = input[p:(p+3)]
-        p+=3 #see?
-
+file = open("C:\\Users\\Owner\\Desktop\\The-Ultimate-Image-Archives-master\\The-Ultimate-Image-Archives-master\\arrayCont.txt" , "r")
+line = file.readline()
 
 img = Image.new("RGB", (width, height))
 pixles = img.load()
 
 for i in range(width):
     for j in range(height):
-        pixles[i, j] = (arr[i*j][0], arr[i*j][1], arr[i*j][2]) #does this work? no. no it doesn't
+        
+        pixles[i, j] = (int(line[(i*j)*4:(i*j)*4+4]), int(line[(i*j)*4+4:(i*j)*4+8]), int(line[(i*j)*4+8:(i*j)*4+12])) #does this work?
 
 img.show()
